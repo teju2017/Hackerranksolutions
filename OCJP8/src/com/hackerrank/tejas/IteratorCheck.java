551e577a83d7a4eee971976b2eb06c1785a79272
teju <teju.hm@practice.com>
2020-05-23 20:08:26 +0800

    committed

---
+++ OCJP8/src/com/hackerrank/tejas/IteratorCheck.java
@@ -0,0 +1,15 @@
+package com.hackerrank.tejas;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class IteratorCheck<T> {
+	
+	
+   public void check(T x)
+   {
+	   System.out.println(" Type t is "+x);
+   }
+
+}
